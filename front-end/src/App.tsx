adaa079aedf948150a5835dca96fd0b97b30857f
shashikant kumar <shashi94goswami@gmail.com>
2024-10-09 21:47:53 +0530

    forntend

---
+++ front-end/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className=" text-center text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
